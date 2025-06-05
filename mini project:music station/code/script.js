d3d27e30ca0a7a2ff00a9ff791d6c55d3d3541cd
Vedant khopade <81235820+TECHNOCRAFT27@users.noreply.github.com>
2025-06-05 07:40:42 +0530

    Delete mini project:music station/code directory

--- mini project:music station/code/script.js
+++
@@ -1,8 +0,0 @@
-// Run code editor preview
-document.getElementById('runBtn').addEventListener('click', () => {
-  const html = document.getElementById('htmlCode').value;
-  const css = `<style>${document.getElementById('cssCode').value}</style>`;
-  const iframe = document.getElementById('preview');
-  iframe.srcdoc = css + html;
-});
-
